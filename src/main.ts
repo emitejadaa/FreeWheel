d2a8e279f9497abc77ec7a00c88c5c729a39afae
emitejadaa <emitejadaaragon@gmail.com>
2026-05-11 15:44:57 -0300

    Refactor bootstrap to use createServer directly
    Refactor the bootstrap function to use createServer directly and remove NestFactory. Update CORS configuration and logging.

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,12 @@
-import { NestFactory } from '@nestjs/core';
 import { createServer } from './app.factory';
-import { ValidationPipe } from '@nestjs/common';
-import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = createServer();
-  // const app = await NestFactory.create(AppModule);
+  const server = createServer();
+  const port = process.env.PORT ?? 3000;
 
-  app.enableCors({
-    origin: true,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    allowedHeaders: '*',
-    credentials: true,
+  server.listen(port, () => {
+    console.log(`Server running on port ${port}`);
   });
-
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
-      transform: true,
-    }),
-  );
-
-  await app.listen(process.env.PORT ?? 3000);
 }
 
 bootstrap();
