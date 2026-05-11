d40abbbc53bbd6964f724e0d4b7225ffdce0db73
IgnacioBritoss <167232030+IgnacioBritoss@users.noreply.github.com>
2026-05-11 17:48:40 -0300

    nada

--- prisma/migrations/20260510000000_add_accepted_terms/migration.sql
+++ prisma/migrations/20260510000000_add_accepted_terms/migration.sql
@@ -1,11 +1,4 @@
--- Add Google OAuth to User
-ALTER TABLE "User"
-  ADD COLUMN "googleId" TEXT;
-
+ALTER TABLE "User" ADD COLUMN "googleId" TEXT;
 CREATE UNIQUE INDEX "User_googleId_key" ON "User"("googleId");
-
--- Add purpose to VerificationCode
 CREATE TYPE "VerificationCodePurpose" AS ENUM ('EMAIL_VERIFICATION', 'PASSWORD_RESET');
-
-ALTER TABLE "VerificationCode"
-  ADD COLUMN "purpose" "VerificationCodePurpose" NOT NULL DEFAULT 'EMAIL_VERIFICATION';
+ALTER TABLE "VerificationCode" ADD COLUMN "purpose" "VerificationCodePurpose" NOT NULL DEFAULT 'EMAIL_VERIFICATION';
